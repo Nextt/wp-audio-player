c0485253c0592606b5fef06df54dacbddbe797fc
Tom McFarlin <tom@tommcfarlin.com>
2013-02-07 23:07:16 -0500

    Waiting for everything to load before loading the player.

--- js/dev/plugin.js
+++ js/dev/plugin.js
@@ -3,9 +3,13 @@
 	
 	// Wait until after everything is loaded
 	$(window).load(function() {
-	
-		$( '.wp-audio-player' ).audioPlayer();
+
+		// After that, if the user is using Firefox, we need to force the player to display
+		if( $.browser.mozilla ) {
+			$( 'audio.wp-audio-player' ).css( 'display', 'block' );
+		} // end if
+		
+		$( 'audio.wp-audio-player' ).audioPlayer();
 		
 	});
-	
 }(jQuery));
